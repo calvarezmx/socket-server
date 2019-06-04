169e2458d71782a2742068fdf6edf9862a86e9d6
Carlos Álvarez <carlos.alvarezs@gmodelo.com.mx>
2019-06-04 14:44:23 -0500

    Sección 4

--- classes/server.ts
+++ classes/server.ts
@@ -1,17 +1,50 @@
 import express  from 'express';
 import { SERVER_PORT } from '../global/environment';
+import socketIO from 'socket.io';
+import http from 'http';
+
+import * as socket from '../sockets/sockets';
 
 export default class Server {
 
+  private static _instance: Server;
+
   public app: express.Application;
   public port: number;
+  public io: socketIO.Server;
+  public httpServer: http.Server;
   
-  constructor() {
+  private constructor() {
     this.app = express();
-    this.port = SERVER_PORT
+    this.port = SERVER_PORT;
+    this.httpServer = new http.Server( this.app );
+    this.io = socketIO( this.httpServer );
+    this.escucharSockets();
   }
 
-  start( callback: Function) {
-    this.app.listen( this.port, callback);
+  public static get instance() {
+    return this._instance || ( this._instance = new this() );
+  }
+
+  private escucharSockets () {
+    console.log('escuchar sockets');
+    this.io.on('connection', (client) => {
+      console.log('Cliente conectado');
+
+      // Mensajes
+      socket.mensaje( client, this.io );
+
+      // Desconectar
+      socket.desconectar( client );
+
+      // client.on('disconnect', () => {
+      //   console.log('cliente desconectado');
+      // });
+    });
+  }
+
+  start( callback?: Function ) {
+    // this.app.listen( this.port, callback);
+    this.httpServer.listen( this.port, callback );
   }
 }
--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import router from './routes/router';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
-const server = new Server();
+const server = Server.instance;
 
 // BodyParser
 server.app.use( bodyParser.urlencoded({ extended: true }) );
